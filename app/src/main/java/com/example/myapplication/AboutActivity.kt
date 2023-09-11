cd7133c8da72bdddc2c1c49a3a3686dad0f7be3b
andreww44 <72322317+andreww44@users.noreply.github.com>
2023-09-11 19:34:32 -0300

    Añadiendo las clases

--- app/src/main/java/com/example/myapplication/AboutActivity.kt
+++ app/src/main/java/com/example/myapplication/AboutActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val intentMain = Intent(this, MainActivity::class.java)
+
+        val boton = findViewById<Button>(R.id.boton2)
+
+        boton.setOnClickListener{
+            startActivity(intentMain);
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,17 +4,20 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //HOME SCREEN
-        //Intent intento = new Intent(packageContext: MainActivity.this, AboutActivity.class)
-
         val intentAbout = Intent(this, AboutActivity::class.java)
-        //startActivity(intentAbout);
+
+        val boton1 = findViewById<Button>(R.id.boton1)
+
+        boton1.setOnClickListener{
+            startActivity(intentAbout);
+        }
 
 
     }
