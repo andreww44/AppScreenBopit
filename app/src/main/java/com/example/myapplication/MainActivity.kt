c8d285d26130b431746e0c95ce925530d5ed7e8c
andreww44 <72322317+andreww44@users.noreply.github.com>
2023-09-15 00:16:37 -0300

    Añadiendo unas cosiñas

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Intent about
         val intentAbout = Intent(this, AboutActivity::class.java)
+        val aboutButton = findViewById<Button>(R.id.About)
 
-        val boton1 = findViewById<Button>(R.id.boton1)
-
-        boton1.setOnClickListener{
+        aboutButton.setOnClickListener{
             startActivity(intentAbout);
         }
 
+        //Intent Exit
+
+        val exitButton = findViewById<Button>(R.id.About)
 
     }
+
+    @SuppressLint("")
+
 }
