5d9af345d48f5c308f725c8e91f9ac74464a924a
andreww44 <72322317+andreww44@users.noreply.github.com>
2023-09-16 14:30:06 -0300

    Beta1

--- app/src/main/java/com/example/myapplication/Preferencias.kt
+++ app/src/main/java/com/example/myapplication/Preferencias.kt
@@ -3,15 +3,42 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+import kotlin.system.exitProcess
 
 class Preferencias : AppCompatActivity() {
+
+    val u2 = "Alvaro"
+    val p2 = "1234"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferencias)
 
         setSupportActionBar(findViewById(R.id.toolbar2))
+
+        val loginButton = findViewById<Button>(R.id.Login)
+
+        val user = findViewById<EditText>(R.id.User);
+        val password = findViewById<EditText>(R.id.Password);
+        val userlog = findViewById<TextView>(R.id.UserLog);
+
+        loginButton.setOnClickListener{
+            if(user.getText().toString().equals(u2.toString()) && password.getText().toString().equals(p2.toString())){
+                userlog.setText("Score: 2000")
+            }
+            else
+            {
+                userlog.setText("Contraseña Incorrecta")
+            }
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
